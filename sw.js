e7ab6bf633577f8250637e488d4538b6f3f10b34
rafagund <153012480+rafagund@users.noreply.github.com>
2026-05-27 18:04:43 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -23,50 +23,6 @@ rafaelfagundes@MacBook-Pro---Rafael-Fagundes ~ % echo '{
     }
   ]
 }' | pbcopy
-rafaelfagundes@MacBook-Pro---Rafael-Fagundes ~ % 
-  
-Last login: Wed May 27 18:00:43 on ttys000
-rafaelfagundes@MacBook-Pro---Rafael-Fagundes ~ % echo 'const CACHE = "resumo-aula-v1";
-const ASSETS = [
-  "/resumo-aula/",
-  "/resumo-aula/index.html",
-  "/resumo-aula/manifest.json",
-  "/resumo-aula/icon-192.png",
-  "/resumo-aula/icon-512.png"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
-});' | pbcopy
-rafaelfagundes@MacBook-Pro---Rafael-Fagundes ~ % 
-
-
-
-
-
-
-
-
-
-
 
 
 
